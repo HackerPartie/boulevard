9081f969fc816950262ad1b78f4eb4b3ffbbaac0
jens <jens@service-b.org>
2014-01-28 13:33:39 +0100

    the servlet is randomly on the way

--- src/main/java/hacker/partie/controller/SentenceServlet.java
+++ src/main/java/hacker/partie/controller/SentenceServlet.java
@@ -1,11 +1,29 @@
 package hacker.partie.controller;
 
+import hacker.partie.databasePackage.SentenceDB;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/sentence")
 public class SentenceServlet extends HttpServlet {
 	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String sentence = SentenceDB.createRandomSentence().toString();
+		request.setAttribute("sentence", sentence);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+		dispatcher.forward(request, response);
+		
+	}
 	
 
 }
