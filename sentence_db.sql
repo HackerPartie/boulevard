412e8b3d78f366af3fa615590f3841c56d510af7
Emmanuel Kasper <emmanuel@libera.cc>
2014-04-05 12:09:22 +0200

    move database setup to postgresql synthax

--- sentence_db.sql
+++ sentence_db.sql
@@ -1,24 +1,22 @@
+DROP TABLE IF EXISTS sentences_svo;
 
+-- create a table containing our Subject - Verb - Object sentences
+CREATE TABLE sentences_svo(
+id serial primary key, object VARCHAR(100), verb VARCHAR(100), complement VARCHAR(100));
 
-DROP TABLE IF EXISTS sentences;
+INSERT INTO sentences_svo VALUES (default, 'Alko-Papa', 'zwang' , 'Sohn zum Autofahren');
 
-CREATE TABLE sentences(
-id int not null auto_increment, object VARCHAR(100), verb VARCHAR(100), complement VARCHAR(100), PRIMARY KEY(id));
+INSERT INTO sentences_svo VALUES (default, 'Mann', 'entreisst' , 'Polizist Pistole und schiesst');
 
+INSERT INTO sentences_svo VALUES (default, 'Grossbrand', 'zerstoert' , 'UNESCO-Weltkulturerbe');
 
-INSERT INTO sentences VALUES (default, 'Alko-Papa', 'zwang' , 'Sohn zum Autofahren');
+INSERT INTO sentences_svo VALUES (default, 'Islamist', 'droht' , 'mit Olympia Terror');
 
-INSERT INTO sentences VALUES (default, 'Mann', 'entreisst' , 'Polizist Pistole und schiesst');
+INSERT INTO sentences_svo VALUES (default, 'Beyonce', 'versext' , 'die Grammys');
 
-INSERT INTO sentences VALUES (default, 'Grossbrand', 'zerstoert' , 'UNESCO-Weltkulturerbe');
+INSERT INTO sentences_svo VALUES (default, 'Bim-Fahrer', 'überlebt' , 'ihre Attacke');
 
-INSERT INTO sentences VALUES (default, 'Islamist', 'droht' , 'mit Olympia Terror');
-
-INSERT INTO sentences VALUES (default, 'Beyonce', 'versext' , 'die Grammys');
-
-INSERT INTO sentences VALUES (default, 'Bim-Fahrer', 'überlebt' , 'ihre Attacke');
-
-INSERT INTO sentences VALUES (default, 'Mama', 'rettete' , 'Sohn vor dem Ertrinken');
+INSERT INTO sentences_svo VALUES (default, 'Mama', 'rettete' , 'Sohn vor dem Ertrinken');
 
 
 
