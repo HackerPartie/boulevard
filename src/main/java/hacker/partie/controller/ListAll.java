43d1575497316c9c3ce58cc6cbd77cabe79180d5
Emmanuel Kasper <emmanuel@libera.cc>
2014-04-21 17:36:15 +0200

    Refactor URL to following pattern:
    /random for random sentence
    /add for add formular
    /listall list all the original sentences already entered

---
+++ src/main/java/hacker/partie/controller/ListAll.java
@@ -0,0 +1,31 @@
+package hacker.partie.controller;
+
+import hacker.partie.model.Sentence;
+import hacker.partie.model.SentenceDB;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/listall")
+public class ListAll extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		List<Sentence> fullList = SentenceDB.findAll();
+		request.setAttribute("fullList", fullList);
+
+		RequestDispatcher dispatcher = request
+				.getRequestDispatcher("listall.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
--- src/main/java/hacker/partie/controller/Random.java
+++ src/main/java/hacker/partie/controller/Random.java
@@ -12,22 +12,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/sentence")
-public class SentenceServlet extends HttpServlet {
-	
+@WebServlet("/random")
+public class Random extends HttpServlet {
+
 	private static final long serialVersionUID = 1L;
 
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 
 		Sentence sentence = SentenceDB.createRandom();
 		request.setAttribute("randomJunk", sentence);
-		
-		RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+
+		RequestDispatcher dispatcher = request
+				.getRequestDispatcher("index.jsp");
 		dispatcher.forward(request, response);
-		
+
 	}
-	
 
 }
