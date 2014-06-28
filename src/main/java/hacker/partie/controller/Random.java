5c6accbc1febc222400e10854535a5503e706301
Emmanuel Kasper <emmanuel@libera.cc>
2014-06-28 22:18:53 +0200

    remove version from generated war file

--- src/main/java/hacker/partie/controller/Random.java
+++ src/main/java/hacker/partie/controller/Random.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/random")
+@WebServlet("/")
 public class Random extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
